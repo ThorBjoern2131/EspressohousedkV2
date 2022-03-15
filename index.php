5adfdb48dbb6ef811a28db325bcc6efed77912fd
simon <simo212a@edu.zealand.dk>
2022-03-15 13:09:46 +0100

    Meta Property Open Graph

--- index.php
+++ index.php
@@ -18,6 +18,12 @@
     <meta name="author" content="Udgiver">
     <meta name="copyright" content="Information om copyright">
     <meta name="keywords" content="Espresso house,">
+    <meta property="og:title" content="Espresso house - Passionate About Coffe"/>
+    <meta property="og:type" content="website"/>
+    <meta property="og:url" content="https://www.EspressohousedkV2.dk"/>
+    <meta property="og:image" content=""/>
+    <meta property="og:description" content=""/>
+    <meta property="og:locale" content="da_DK"
     <!-- Sikrer man kan benytte CSS ved at tilkoble en CSS fil -->
     <link href="css/styles.scss" rel="stylesheet" type="text/css">
 
