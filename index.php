2e137e5791ac02c53de44ba4bb3cd9182755004d
madsr <madsrosendal@live.dk>
2022-03-17 16:18:35 +0100

    Hej

--- index.php
+++ index.php
@@ -44,8 +44,8 @@
 
 
 <div class="topnav">
-    <a href="index.php" style="font-size:30px;">Om os</a>
-    <a href="OmOs.php" style="font-size:30px;">App</a>
+    <a href="index.php" style="font-size:30px;">Home</a>
+    <a href="OmOs.php" style="font-size:30px;">Om os</a>
     <a href="App.php" style="font-size:30px;">kaffebarer</a>
     <a href="Kaffebarer.php" style="font-size:30px;">App</a>
 </div>
